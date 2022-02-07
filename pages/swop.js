c4489b8708bf92d3f01db61a5ea6f43de00531bf
Joe Fitter <hello@joefitter.com>
2022-02-07 18:54:50 +0100

    replace locahost with env var

--- pages/swop.js
+++ pages/swop.js
@@ -39,7 +39,7 @@ export default function Swop() {
   async function updateTokenBalance() {
     if (wallet && wallet.publicKey) {
       const key = wallet.publicKey.toString();
-      const response = await axios.get(`http://localhost:3000/api/get-token-balance/${key}`);
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/get-token-balance/${key}`);
 
       const { amount } = response.data;
 
