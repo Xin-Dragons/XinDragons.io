b8544ffe40b7cc69d8c94566f8092db156b86d4a
Joe Fitter <hello@joefitter.com>
2022-03-02 17:21:40 +0100

    updated service worker

--- pages/api/run-snapshot.js
+++ pages/api/run-snapshot.js
@@ -1,4 +1,6 @@
 import { map, flatten, reduce, omit } from 'lodash';
+import { fork } from 'child_process';
+
 import takeSnapshot from '../../lib/take-snapshot';
 import ingest from '../../lib/ingest-data';
 
@@ -8,20 +10,22 @@ export default async function handler(req, res) {
       const { authorization } = req.headers;
 
       if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
-        console.log('AUTHORIZED')
-        const snapshot = await takeSnapshot();
-        await ingest(snapshot);
+        try {
+          fork('workers/collection.worker.js', { cwd: process.cwd() });
+        } catch(e) {
+          console.error(e);
+          return res.status(500).json({});
+        }
+
         res.status(200).json({ success: true });
+
       } else {
-        console.log('UNAUTHORIZED')
         res.status(401).json({ success: false });
       }
     } catch (err) {
-      console.log('THROWN', err.message)
       res.status(500).json({ statusCode: 500, message: err.message });
     }
   } else {
-    console.log('BAD METHOD')
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
