acf2ddedac68a75cf8d01c763743976015128dcc
Joe Fitter <hello@joefitter.com>
2022-03-03 11:12:58 +0100

    add try catch to worker

--- workers/collection.worker.js
+++ workers/collection.worker.js
@@ -2,8 +2,12 @@ const takeSnapshot = require('../lib/take-snapshot');
 const ingest = require('../lib/ingest-data');
 
 (async () => {
-  const snapshot = await takeSnapshot();
-  console.log(`Snapshot taken`)
-  await ingest(snapshot);
-  console.log('Data ingested')
+  try {
+    const snapshot = await takeSnapshot();
+    console.log(`Snapshot taken`)
+    await ingest(snapshot);
+    console.log('Data ingested')
+  } catch (err) {
+    console.error('FAILED!!!!!!!!!!!!', err);
+  }
 })();
