e4d05ec0fef8107f4efc8b571c8cd4d14e5154c7
Joe Fitter <hello@joefitter.com>
2022-03-03 12:38:20 +0100

    moved worker

--- next.config.js
+++ next.config.js
@@ -16,18 +16,18 @@ module.exports = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (isServer) {
-      const entry = config.entry;
-
-      config.entry = () => {
-        return entry().then((entry) => {
-          return Object.assign({}, entry, { 'collection.worker': path.resolve(process.cwd(), 'workers/collection.worker.js') })
-        })
-      }
-    }
-
-
-    return config;
-  }
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   if (isServer) {
+  //     const entry = config.entry;
+  //
+  //     config.entry = () => {
+  //       return entry().then((entry) => {
+  //         return Object.assign({}, entry, { 'collection.worker': path.resolve(process.cwd(), 'workers/collection.worker.js') })
+  //       })
+  //     }
+  //   }
+  //
+  //
+  //   return config;
+  // }
 };
