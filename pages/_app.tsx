f13273bf814e7ca1f0c8b1b248070c55e9e739c2
Aleksandar Kolov <alex@xindragons.io>
2023-10-25 17:03:27 +0200

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,16 @@
-import * as anchor from '@project-serum/anchor';
 
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
+
+
 import { AppProps } from 'next/app';
 import { FC, useMemo } from 'react';
 
 import { Layout } from '../components';
 
-// Use require instead of import since order matters
-require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 
 
-const network = process.env.NEXT_PUBLIC_NETWORK as WalletAdapterNetwork;
-const rpcHost = process.env.NEXT_PUBLIC_RPC_HOST!;
-const connection = new anchor.web3.Connection(rpcHost
-  ? rpcHost
-  : anchor.web3.clusterApiUrl('devnet'));
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.Mainnet;
 
 
   return (
